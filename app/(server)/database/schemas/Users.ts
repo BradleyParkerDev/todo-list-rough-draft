56106ed3451658d82510655ed504dc7fe4992aae
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-05-21 02:21:53 -0500

    updated Users schema

--- app/(server)/database/schemas/Users.ts
+++ app/(server)/database/schemas/Users.ts
@@ -1,5 +1,5 @@
 // database/schemas/users.js
-import  { pgTable, text, uuid, timestamp }  from 'drizzle-orm/pg-core';
+import  { pgTable, text, uuid, timestamp, PgArray }  from 'drizzle-orm/pg-core';
 
 const User = pgTable('users', {
 	id: uuid('id').primaryKey().defaultRandom().unique(), // UUID v4 primary key
@@ -7,8 +7,7 @@ const User = pgTable('users', {
 	lastName: text('last_name').notNull(), // Not nullable text field
 	emailAddress: text('email_address').unique().notNull(), // Unique and not nullable
 	userName: text('user_name').unique().notNull(), // Unique and not nullable
-
-	userImage: text('user_image').default(''), // Optional field with default empty string
+	refreshTokens: text('refresh_tokens'), // Define as array of text	userImage: text('user_image').default(''), // Optional field with default empty string
 	password: text('password').notNull(), // Password field
 	lastUpdated: timestamp('last_updated').defaultNow().notNull() // Automatically sets current timestamp on update
 });
